2a9d9454b9d0ce508e118a198615c1b0336d0b72
Vitor Paladini <paladinivitor.vp@gmail.com>
2023-03-21 08:35:31 -0300

    Edit

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
+//DEFININDO O TIPO STRUCT PARA ARMANEZAR AS PESSOAS
 struct Pessoa {
     char nome[100];
     int idade;
@@ -8,11 +9,45 @@ struct Pessoa {
     int imunizado;
 };
 
+struct Pessoa pessoas[5];
+
+//CRIANDO O ARRAY FILA PARA ORDENAR AS PESSOAS À SEREM IMUNIZADAS
+char fila[5][100];
+int fim_fila = 1;
+
+#define TAMANHO_FILA 5
+
+//CRIANDO VOIDS PARA ADICIONAR E REMOVER PESSOAS DA FILA
+void adicionar_pessoa_na_fila() {
+  
+    if (fim_fila <= TAMANHO_FILA) {
+        strcpy(fila[fim_fila], pessoas[fim_fila].nome);
+        fim_fila++;
+    }
+}
+
+void remover_pessoa_da_fila() {
+    if (fim_fila > 0) {
+        for (int i = 1; i < fim_fila - 1; i++) {
+            strcpy(fila[i], fila[i+1]);
+      }
+        fim_fila--;
+    }
+}
+
+void imprimir_fila() {
+    for (int i = 1; i <= fim_fila; i++) {
+        printf("%d - %s\n", i, fila[i]);
+    }
+}
+
+//APRESENTANDO O PROGRAMA
 int main(void) {
   printf("\nCRIADO POR: VITOR PALADINI\n\nGHS\n\nCom o GHS (Gestão Hospitalar Simplifica), você pode facilmente organizar e gerenciar o seu hospital de forma mais eficiente. Desfrute de uma plataforma intuitiva e funcional que ajudará a simplificar processos e melhorar a qualidade do atendimento aos pacientes.\n\n\n");
 
   printf("Por favor, informe a capacidade total da sala de triagem, ou seja, o número máximo de pessoas que podem ser acomodadas no local:\n\nWARNING: LIMITE MIN 3 PESSOAS, MÁX.5 PESSOAS\n");
 
+//SCANF DO MÁXIMO DA FILA DE TRIAGEM E DOSES
   int total_triagem;
 
   scanf("%d", &total_triagem);
@@ -44,11 +79,10 @@ int fila=1;
 
   switch (menu) {
 
+//CADASTRAR INTERESSADO
     case 1:
-    
-    struct Pessoa pessoas[5];
 
-    for (int i=interessados; i < total_triagem; i++, interessados++) {
+    for (int i=1; i <= total_triagem; i++) {
 
         printf("\nInforme o nome da pessoa:\n");
         scanf("%s", &pessoas[i].nome);
@@ -59,11 +93,11 @@ int fila=1;
         printf("\nInforme o sexo da pessoa (M/F):\n");
         scanf("%s", &pessoas[i].sexo);
 
+        adicionar_pessoa_na_fila();
+
         printf("\nGostaria de cadastrar outro interessado? (1-S/2-N):\n");
         scanf("%d", &continuar);
         pessoas[i].imunizado=0;
-
-        int interessados=i;
 
         if(continuar==2){
           break;
@@ -78,10 +112,23 @@ int fila=1;
     int j=1;
     if(doses>0){
 
-        for (int i=fila; i < total_triagem; i++,fila++){
+        for (int i=fila; i < total_triagem; i++){
 
           if(pessoas[i].imunizado==0){
+
             printf("\nSeguindo a fila, %s foi imunizado(a)!\n", pessoas[i].nome);
+
+            remover_pessoa_da_fila();
+            doses--;
+
+              if (doses <= 0){
+
+                printf("As doses do seu hospital, acabaram!");
+
+                break;
+
+              }
+
             printf("\nGostaria de continuar a fila de imunização?(1-S/2-N)\n");
             scanf("%d", &continuar);
 
@@ -114,6 +161,11 @@ int fila=1;
       break;
     
     case 3:
+
+      printf("Imprimindo fila de interessados:\n\n");
+
+        imprimir_fila();
+
       break;
 
     case 4:
